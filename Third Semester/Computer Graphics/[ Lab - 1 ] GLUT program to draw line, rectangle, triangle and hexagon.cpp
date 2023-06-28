100b0a725127da267f5a0fc5c116ccad1d540bd7
Swarup Dahal <118110852+swarrup17@users.noreply.github.com>
2023-06-28 15:18:32 +0545

    Update [ Lab - 1 ] GLUT program to draw line, rectangle, triangle and hexagon.cpp

--- Third Semester/Computer Graphics/[ Lab - 1 ] GLUT program to draw line, rectangle, triangle and hexagon.cpp
+++ Third Semester/Computer Graphics/[ Lab - 1 ] GLUT program to draw line, rectangle, triangle and hexagon.cpp
@@ -8,30 +8,30 @@ void drawShapes()
     // Draw line
     glColor3f(1.0, 0.0, 0.0); // Red
     glBegin(GL_LINES);
-    glVertex2f(-0.8, 0.8);
-    glVertex2f(0.8, -0.8);
+    glVertex2f(-0.8, 0.05);
+    glVertex2f(0.0, -0.4);
     glEnd();
 
     // Draw a rectangle
     glColor3f(0.0, 1.0, 0.0); // Green
     glBegin(GL_LINE_LOOP);
-    glVertex2f(-0.7, -0.5); // Bottom-left vertex
-    glVertex2f(-0.1, -0.5); // Bottom-right vertex
-    glVertex2f(-0.1, 0.5);  // Top-right vertex
-    glVertex2f(-0.7, 0.5);  // Top-left vertex
+    glVertex2f(-0.9, 0.1); // Bottom-left vertex
+    glVertex2f(-0.5, 0.1); // Bottom-right vertex
+    glVertex2f(-0.5, 0.5);  // Top-right vertex
+    glVertex2f(-0.9, 0.5);  // Top-left vertex
     glEnd();
 
     // Draw a triangle
     glColor3f(0.0, 0.0, 1.0); // Blue
-    glBegin(GL_TRIANGLES);
-    glVertex2f(-0.3, -0.5);
-    glVertex2f(0.0, 0.5);
-    glVertex2f(0.3, -0.5);
+   glBegin(GL_LINE_LOOP);
+    glVertex2f(-0.1,0.1);
+    glVertex2f(-0.4, 0.1);
+    glVertex2f(0.25, 0.4);
     glEnd();
 
     // Draw a hexagon
     glColor3f(1.0, 1.0, 0.0); // Yellow
-    glBegin(GL_POLYGON);
+    glBegin(GL_LINE_LOOP);
     glVertex2f(0.1, 0.0);   // Bottom-left vertex
     glVertex2f(0.35, 0.5);  // Top-left vertex
     glVertex2f(0.65, 0.5);  // Top vertex
@@ -50,8 +50,8 @@ int main(int argc, char* argv[])
     glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
     glutInitWindowSize(800, 600); // Increase window size
     glutInitWindowPosition(100, 100);
-    glutCreateWindow("Drawing Shapes");
-    glClearColor(0.0, 0.0, 0.0, 1.0); // Set clear color to black
+    glutCreateWindow("Swarup Dahal");
+    glClearColor(1.0, 1.0, 1.0, 1.0); // Set clear color to black
     glutDisplayFunc(drawShapes);
     glutMainLoop();
     return 0;
